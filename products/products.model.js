5c9be3c86755ff568a85c331a32a9cb0c0cb7ad4
MarkoErdelji <erdeljimarko@gmail.com>
2022-09-23 18:55:14 +0200

    feat: Added mutation to add products

--- products/products.model.js
+++ products/products.model.js
@@ -3,16 +3,30 @@ const products = [
         id: 'redshoe',
         description: 'Red Shoe',
         price: 42.12,
+        reviews: []
     },
     {
         id:'bluejean',
         description: 'Blue Jeans',
         price: 55.55,
+        reviews: []
     }
 ]
 
 function getAllProducts(){
     return products;
+}
+
+function addNewProduct(id,description,price){
+    const newProduct = 
+        {
+            id: id,
+            description: description,
+            price:price,
+            reviews: []
+        }
+    products.push(newProduct)
+    return newProduct
 }
 
 function getProductById(id){
@@ -28,5 +42,6 @@ function getProductsByPrice(min,max){
 module.exports = {
     getAllProducts,
     getProductsByPrice,
-    getProductById
+    getProductById,
+    addNewProduct
 }
